3925f787f28fcbf61a91c4fcbc3374489411494d
Kamil Raczycki <raczyckikamil@gmail.com>
2025-07-28 23:49:46 +0200

    chore: pass env variables to codecov

--- tests/low_resources/run_tests.sh
+++ tests/low_resources/run_tests.sh
@@ -1,5 +1,8 @@
-coverage run --data-file=.coverage.base.tests --source=rq_geo_toolkit -m pdm run pytest -v -s --durations=20 test_sorting.py
+echo $GITHUB_SHA
+coverage run --data-file=.coverage.base.tests --source=rq_geo_toolkit -m \
+    pdm run pytest -v -s --durations=20 test_sorting.py
 coverage combine
 coverage xml -o coverage.xml
 coverage report -m
-codecov --verbose upload-process --disable-search --fail-on-error coverage.xml
+codecov --verbose upload-process --disable-search --fail-on-error \
+    -F low-resources-test -f coverage.xml -C $GITHUB_SHA -t $CODECOV_TOKEN
