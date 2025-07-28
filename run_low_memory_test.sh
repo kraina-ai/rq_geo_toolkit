58b3f8947a8cebf948a4a429b0dd635ffd3da374
Kamil Raczycki <raczyckikamil@gmail.com>
2025-07-28 23:38:35 +0200

    ci: remove -it flag

--- run_low_memory_test.sh
+++ run_low_memory_test.sh
@@ -1,5 +1,5 @@
 docker build --progress plain -f tests/low_resources/Dockerfile -t rq-geo-toolkit_low-resources-test .
-docker run -it --rm --memory=512m -v "$(pwd)/files:/app/files" \
+docker run --rm --memory=512m -v "$(pwd)/files:/app/files" \
     -e GITHUB_ACTION -e GITHUB_RUN_ID -e GITHUB_REF -e GITHUB_REPOSITORY \
     -e GITHUB_SHA -e GITHUB_HEAD_REF -e CODECOV_TOKEN \
     rq-geo-toolkit_low-resources-test /bin/bash /app/run_tests.sh
