b30b82d394f19805f8d0d2f3d6c1d0184ee707f1
Kamil Raczycki <raczyckikamil@gmail.com>
2025-08-20 10:52:50 +0200

    feat: add option to run duckdb query with existing connection

--- rq_geo_toolkit/duckdb.py
+++ rq_geo_toolkit/duckdb.py
@@ -2,6 +2,7 @@
 
 import tempfile
 from collections.abc import Callable
+from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from math import ceil
 from pathlib import Path
@@ -21,6 +22,7 @@ if TYPE_CHECKING:  # pragma: no cover
 
 
 DUCKDB_ABOVE_130 = version.parse(duckdb.__version__) >= version.parse("1.3.0")
+
 
 def sql_escape(value: str) -> str:
     """Escape value for SQL query."""
@@ -132,19 +134,104 @@ def run_duckdb_query_function_with_memory_limit(
 
 def run_query_with_memory_monitoring(
     sql_query: str,
-    tmp_dir_path: Path,
+    tmp_dir_path: Optional[Path] = None,
+    connection: Optional[duckdb.DuckDBPyConnection] = None,
     verbosity_mode: "VERBOSITY_MODE" = "verbose",
     preserve_insertion_order: bool = False,
 ) -> None:
-    """Run SQL query and raise exception if memory threshold is exceeded."""
-    run_duckdb_query_function_with_memory_limit(
-        tmp_dir_path=tmp_dir_path,
-        verbosity_mode=verbosity_mode,
-        current_memory_gb_limit=None,
-        current_threads_limit=None,
-        function=_run_query,
-        args=(sql_query, preserve_insertion_order),
-    )
+    """
+    Run SQL query and raise exception if memory threshold is exceeded.
+
+    Will run in external process or use existing DuckDB connection.
+
+    Args:
+        sql_query (str): Query to be executed.
+        tmp_dir_path (Path, optional): Directory where to create a new DuckDB connection.
+            Defaults to None.
+        connection (duckdb.DuckDBPyConnection, optional): Existing connection to reuse.
+            Defaults to None.
+        verbosity_mode (VERBOSITY_MODE, optional): Log level. Defaults to "verbose".
+        preserve_insertion_order (bool, optional): Whether to keep operations in order.
+            Used only with external process. Defaults to False.
+    """
+    if tmp_dir_path is None and connection is None:
+        raise ValueError("Must pass tmp_dir_path or connection.")
+
+    if tmp_dir_path is not None and connection is not None:
+        raise ValueError("Cannot pass both tmp_dir_path and connection at the same time.")
+
+    if tmp_dir_path is not None:
+        run_duckdb_query_function_with_memory_limit(
+            tmp_dir_path=tmp_dir_path,
+            verbosity_mode=verbosity_mode,
+            current_memory_gb_limit=None,
+            current_threads_limit=None,
+            function=_run_query,
+            args=(sql_query, preserve_insertion_order),
+        )
+    elif connection is not None:
+        current_memory_gb_limit = ceil(psutil.virtual_memory().total / MEMORY_1GB)
+        current_threads_limit = connection.sql(
+            "SELECT current_setting('threads') AS threads"
+        ).fetchone()[0]
+
+        while True:
+            try:
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                    connection.execute(f"SET memory_limit = '{current_memory_gb_limit}GB';")
+                    connection.execute(f"SET threads = {current_threads_limit};")
+
+                    actual_memory = psutil.virtual_memory()
+                    percentage_threshold = 95
+                    if (actual_memory.total * 0.05) > MEMORY_1GB:  # pragma: no cover
+                        percentage_threshold = (
+                            100 * (actual_memory.total - MEMORY_1GB) / actual_memory.total
+                        )
+
+                    query_execution_future = executor.submit(
+                        _run_query_with_existing_connection, sql_query, connection
+                    )
+
+                    sleep_time = 0.1
+                    while query_execution_future.running():
+                        actual_memory = psutil.virtual_memory()
+                        if actual_memory.percent > percentage_threshold:  # pragma: no cover
+                            connection.interrupt()
+                            query_execution_future.cancel()
+                            raise MemoryError()
+
+                        sleep(sleep_time)
+                        sleep_time = min(sleep_time + 0.1, 1.0)
+
+                    query_execution_future.exception()
+
+                    if query_execution_future.cancelled():
+                        raise MemoryError()
+
+            except (duckdb.OutOfMemoryException, MemoryError) as ex:
+                if current_threads_limit == 2 and current_memory_gb_limit < 1:
+                    raise MemoryError("Not enough memory to run the query.") from ex
+                elif current_threads_limit > 2:
+                    # First limit number of CPUs
+                    current_threads_limit = max(2, ceil(current_threads_limit / 2))
+                elif current_memory_gb_limit > 1:
+                    # Next limit memory
+                    current_memory_gb_limit = ceil(current_memory_gb_limit / 2)
+                elif current_memory_gb_limit == 1:
+                    # Reduce memory below 1 GB
+                    current_memory_gb_limit /= 2
+                else:
+                    raise RuntimeError(
+                        "Not expected error during resources checking "
+                        f"({current_memory_gb_limit:.2f}GB, {current_threads_limit} threads)."
+                    ) from ex
+
+                if not verbosity_mode == "silent":
+                    rprint(
+                        f"Encountered {ex.__class__.__name__} during operation."
+                        " Retrying with lower number of resources"
+                        f" ({current_memory_gb_limit:.2f}GB, {current_threads_limit} threads)."
+                    )
 
 
 def _run_query(
@@ -163,3 +250,9 @@ def _run_query(
         conn.execute(f"SET memory_limit = '{current_memory_gb_limit}GB';")
         conn.execute(f"SET threads = {current_threads_limit};")
         conn.sql(sql_query)
+
+
+def _run_query_with_existing_connection(
+    sql_query: str, connection: duckdb.DuckDBPyConnection
+) -> None:
+    connection.execute(sql_query)
